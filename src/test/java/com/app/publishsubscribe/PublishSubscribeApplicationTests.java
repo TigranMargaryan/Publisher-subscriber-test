5503069ab2a5abd6048ec970fec0f0c565fa0769
Tigran Margaryan <tigran.margaryan@estateguru.co>
2023-01-23 11:39:09 +0400

    Add initial pub-sub app

---
+++ src/test/java/com/app/publishsubscribe/PublishSubscribeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.publishsubscribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublishSubscribeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
